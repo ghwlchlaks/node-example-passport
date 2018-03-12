3e816256fccf072e5a6a98b5bacc9bf5709d0b62
ghwlchlaks <ghwlchlaks@naver.com>
2018-03-12 20:16:38 +0900

    Removed folder from repository : nodejs 안에서 vue 연동 테스트

--- OAuth_passport/Services/AuthenticationServices.js
+++
@@ -1,7 +0,0 @@
-import Api from '@/services/Api'
-
-export default {
-  login (socialName) {
-    return Api().get(`/login/${socialName}`)
-  }
-}
--- OAuth_passport/config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    'social_secret': 'social_account',
-    'social_collection' :'mongodb://localhost/social_account'
-  }
-  
