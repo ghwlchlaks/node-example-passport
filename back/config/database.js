5321971b5c80bbafa5828fbaa5447d11d2f4099b
jiho <ghwlchlaks@naver.com>
2018-12-03 20:52:04 +0900

    old folder delete, nodejs passport module example

--- back/config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    'local_secret': 'school_social_account',
-    'local_collection': 'mongodb://localhost/school_social_account'
-  }
-  
--- front/src/services/AuthenticationService.js
+++
@@ -1,7 +0,0 @@
-import Api from '@/services/Api'
-
-export default {
-  social_login (credentials, socialName) {
-    return Api().post(`/social_login/${socialName}`, credentials)
-  }
-}
